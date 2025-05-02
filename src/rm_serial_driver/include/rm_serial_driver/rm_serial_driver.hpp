0b829b0027d20bc614444a48718b14c4ab0e029a
YueChangxin <auauau@wust.edu.cn>
2025-05-02 19:32:20 +0800

    将 src 目录内容作为普通文件夹提交

---
+++ src/rm_serial_driver/include/rm_serial_driver/rm_serial_driver.hpp
@@ -0,0 +1,102 @@
+// Copyright (c) 2022 ChenJun
+// Licensed under the Apache-2.0 License.
+
+#ifndef RM_SERIAL_DRIVER__RM_SERIAL_DRIVER_HPP_
+#define RM_SERIAL_DRIVER__RM_SERIAL_DRIVER_HPP_
+
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/subscription.hpp>
+#include <serial_driver/serial_driver.hpp>
+#include <std_msgs/msg/float64.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+
+// C++ system
+#include <future>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+#include "auto_aim_interfaces/msg/target.hpp"
+
+namespace rm_serial_driver
+{
+class RMSerialDriver : public rclcpp::Node
+{
+public:
+  explicit RMSerialDriver(const rclcpp::NodeOptions & options);
+
+  ~RMSerialDriver() override;
+
+private:
+  void getParams();
+
+  void receiveData();
+
+  void sendData(auto_aim_interfaces::msg::Target::SharedPtr msg);
+
+  void reopenPort();
+
+  void setParam(const rclcpp::Parameter & param);
+
+  void resetTracker();
+
+  // Serial port
+  std::unique_ptr<IoContext> owned_ctx_;
+  std::string device_name_;
+  std::unique_ptr<drivers::serial_driver::SerialPortConfig> device_config_;
+  std::unique_ptr<drivers::serial_driver::SerialDriver> serial_driver_;
+
+  // Param client to set detect_colr
+  using ResultFuturePtr = std::shared_future<std::vector<rcl_interfaces::msg::SetParametersResult>>;
+  bool initial_set_param_ = false;
+  uint8_t previous_receive_color_ = 0;
+  rclcpp::AsyncParametersClient::SharedPtr detector_param_client_;
+  ResultFuturePtr set_param_future_;
+
+  // Service client to reset tracker
+  rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr reset_tracker_client_;
+
+  // Aimimg point receiving from serial port for visualization
+  visualization_msgs::msg::Marker aiming_point_;
+
+  // Broadcast tf from odom to gimbal_link
+  double timestamp_offset_ = 0;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  rclcpp::Subscription<auto_aim_interfaces::msg::Target>::SharedPtr target_sub_;
+
+  // For debug usage
+  rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr latency_pub_;
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
+
+  std::thread receive_thread_;
+
+  double max_pitch_err_;
+  double max_yaw_err_;
+
+  // 添加弹道解算结果的订阅者
+  rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr projectile_sub_;
+  
+  // 添加存储最新解算结果的变量
+  geometry_msgs::msg::Point latest_solution_;
+  std::mutex solution_mutex_;  // 保护数据访问的互斥锁
+  
+  // 添加回调函数声明
+  void projectileSolutionCallback(const geometry_msgs::msg::Point::SharedPtr msg);
+
+  // 添加 ID 映射表作为类成员
+  const std::map<std::string, uint8_t> id_unit8_map_{
+    {"", 0},  {"outpost", 0}, {"1", 1}, {"2", 2},
+    {"3", 3}, {"4", 4}, {"5", 5}, {"guard", 6}, {"base", 7}
+  };
+};
+}  // namespace rm_serial_driver
+
+#endif  // RM_SERIAL_DRIVER__RM_SERIAL_DRIVER_HPP_
